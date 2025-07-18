47ac6ec67627cc2298e617385c496f338b975caf
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-17 23:09:01 -0400

    add C major scale notes that plays when clicked (new notebutton component)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NoteButton from '@/components/NoteButton';
 import Form from 'next/form'
 import * as Tone from "tone";
 
@@ -11,6 +12,16 @@ function playSampleSound() {
 
 export default function Home() {
   return (
-    <button onClick={playSampleSound}>Play a note!</button>
+    <> 
+      <button onClick={playSampleSound}>Play a note!</button>
+      <NoteButton note={{ noteName: "C", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "D", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "E", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "F", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "G", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "A", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "B", pitch: 4, length: "4n"}}/>
+      <NoteButton note={{ noteName: "C", pitch: 5, length: "4n"}}/>
+    </>
   )
 }
---
+++ src/components/NoteButton.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { JSX } from 'react';
+import * as Tone from "tone";
+
+export interface Note {
+    noteName: string,
+    pitch: number,
+    length: Tone.Unit.Time;
+}
+
+interface NoteButtonProps {
+    note: Note;
+}
+
+function playSound(note : Note): void {
+    const synth = new Tone.Synth().toDestination();
+    
+    synth.triggerAttackRelease(`${note.noteName}${note.pitch}`, note.length);
+}
+
+const NoteButton = ({ note } : NoteButtonProps): JSX.Element => {
+    const { noteName, pitch, length } = note;
+
+    return (
+        <div>
+            <button onClick={() => playSound( {noteName, pitch, length} )}>
+                {noteName}
+            </button>
+        </div>
+    )
+}
+
+export default NoteButton
