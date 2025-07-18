2494dff5745db6c64b6f8ce5ea24d4f2750bcf72
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-18 00:02:51 -0400

    chore: tailwind configuration

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+const config = {
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/assets/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
