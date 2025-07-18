bb9cadd31e0b22133360c5c25ffd8cbeb9afab9e
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-17 23:21:48 -0400

    changed to server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,10 @@
-"use client";
-
 import NoteButton from '@/components/NoteButton';
 import Form from 'next/form'
 import * as Tone from "tone";
 
-function playSampleSound() {
-  const synth = new Tone.Synth().toDestination();
-
-  synth.triggerAttackRelease("D#4", "4n");
-}
-
 export default function Home() {
   return (
     <> 
-      <button onClick={playSampleSound}>Play a note!</button>
       <NoteButton note={{ noteName: "C", pitch: 4, length: "4n"}}/>
       <NoteButton note={{ noteName: "D", pitch: 4, length: "4n"}}/>
       <NoteButton note={{ noteName: "E", pitch: 4, length: "4n"}}/>
