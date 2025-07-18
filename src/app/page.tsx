97b45ccb221fa733d89c651cd67deb171faa091b
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-18 13:22:03 -0400

    put the scale in its own page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
-import NoteButton from '@/components/NoteButton';
-import Form from 'next/form'
-import * as Tone from "tone";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <h1 className="text-center text-5xl py-4">Home Page</h1>
-      <div className="h-50"></div>
-      <div className="flex space-x-5 bg-[#F0E2E7] justify-center items-center h-40"> 
-        <NoteButton note={{ noteName: "C#", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "D#", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "F#", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "G#", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "A#", pitch: 4, length: "8n"}}/>
-      </div>
-      <div className="flex space-x-5 bg-[#F0E2E7] justify-center items-center h-40"> 
-        <NoteButton note={{ noteName: "C", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "D", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "E", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "F", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "G", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "A", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "B", pitch: 4, length: "8n"}}/>
-        <NoteButton note={{ noteName: "C", pitch: 5, length: "8n"}}/>
-      </div>
+      <Link href="/earTraining">
+        Click here!
+      </Link>
     </>
   )
 }
